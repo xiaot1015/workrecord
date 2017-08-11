34f03fdc4582636975545029d57acea3e6e3abf3
xiaotao <xiaotao@didichuxing.com>
2017-08-11 10:59:41 +0800

    fix

---
+++ test/string.c
@@ -0,0 +1,22 @@
+/*************************************************************************
+	> File Name: string.c
+	> Author: 
+	> Mail: 
+	> Created Time: 五  8/11 09:47:51 2017
+ ************************************************************************/
+
+#include<stdio.h>
+int main(){
+    char* command[1] = " set a 60 299";
+    int len;
+    char *e , *c;
+    len = strlen( command );
+
+    e = c command ;
+    for(i=0 ; i< len ;i++) {
+        if( *e == ' ' ){
+            if ()
+        }
+        e++;
+    }
+}
---
+++ test/strlen.c
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: strlen.c
+	> Author: 
+	> Mail: 
+	> Created Time: 五  8/11 10:11:30 2017
+ ************************************************************************/
+
+#include<stdio.h>
+#include<string.h>
+int main() {
+    char a[4] = "1235";
+    printf("len is %d, size is %d\n", strlen(a), sizeof(a));
+    return 1;
+}
