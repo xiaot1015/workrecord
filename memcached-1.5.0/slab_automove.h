bc1a9a666bb244957695a5307e0f243d68e8fe3d
xiaot <xiaot1015@gmail.com>
2017-09-01 13:07:15 +0800

    del

--- memcached-1.5.0/slab_automove.h
+++
@@ -1,8 +0,0 @@
-#ifndef SLAB_AUTOMOVE_H
-#define SLAB_AUTOMOVE_H
-
-void *slab_automove_init(uint32_t window_size, double max_age_ratio);
-void slab_automove_free(void *arg);
-void slab_automove_run(void *arg, int *src, int *dst);
-
-#endif
